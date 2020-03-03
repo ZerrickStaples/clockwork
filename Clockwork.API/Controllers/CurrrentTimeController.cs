08975be309a29b735a414e29e0888491f5809a13
Zerrick Staples <zerrickstaples@zerricks-mbp.home>
2020-03-02 21:08:26 -0500

    Edited display of current time markup upon button click

--- Clockwork.API/Controllers/CurrrentTimeController.cs
+++ Clockwork.API/Controllers/CurrrentTimeController.cs
@@ -6,38 +6,47 @@ using System.Linq;
 
 namespace Clockwork.API.Controllers
 {
-    [Route("api/[controller]")]
     public class CurrentTimeController : Controller
     {
+        [Route("api/[controller]")]
         // GET api/currenttime
         [HttpGet]
         public IActionResult Get()
         {
             var utcTime = DateTime.UtcNow;
-             var serverTime = DateTime.Now;
-             var ip = this.HttpContext.Connection.RemoteIpAddress.ToString();
+            var serverTime = DateTime.Now;
+            var ip = this.HttpContext.Connection.RemoteIpAddress.ToString();
 
-             var returnVal = new CurrentTimeQuery
-             {
-                 UTCTime = utcTime,
-                 ClientIp = ip,
-                 Time = serverTime
-             };
+            var returnVal = new CurrentTimeQuery
+            {
+                UTCTime = utcTime,
+                ClientIp = ip,
+                Time = serverTime
+            };
 
-             using (var db = new ClockworkContext())
-             {
-                 db.CurrentTimeQueries.Add(returnVal);
-                 var count = db.SaveChanges();
-                 Console.WriteLine("{0} records saved to database", count);
+            using (var db = new ClockworkContext())
+            {
+                db.CurrentTimeQueries.Add(returnVal);
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
 
-                 Console.WriteLine();
-                 foreach (var CurrentTimeQuery in db.CurrentTimeQueries)
-                 {
-                     Console.WriteLine(" - {0}", CurrentTimeQuery.UTCTime);
-                 }
-             }
+                Console.WriteLine();
+                foreach (var CurrentTimeQuery in db.CurrentTimeQueries)
+                {
+                    Console.WriteLine(" - {0}", CurrentTimeQuery.UTCTime);
+                }
+            }
 
-             return Ok(returnVal);
+            return Ok(returnVal);
+        }
+        [Route("api/[controller]/all")]
+        //GET api/currenttime/all
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var returnVal = new ClockworkContext().CurrentTimeQueries;
+
+            return Ok(returnVal);
         }
     }
 }
